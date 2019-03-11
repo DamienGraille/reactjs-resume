438d7b7b5bf7ecf5f398b967e5ba6965a9bc7077
Damien1989 <damiengraille@gmail.com>
2019-03-11 21:35:14 +0000

    Create list of personal info
    Create list personal infomation using map to go through array of
    information data. The next problem is to put 2 items on same line so
    they can have different styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-import Header from './components/Work';
-import Header from './components/Projects';
-import Header from './components/Education';
-import Header from './components/Map';
+import Work from './components/Work';
+import Projects from './components/Projects';
+import Education from './components/Education';
+import Map from './components/Map';
 
 class App extends Component {
   render() {
+    var info ={
+     infos:["mobile", "email", "github", "location"],
+     infodata:["07760586545","damiengraille@gmail.com","DamienGraille","London"]
+  };
     return (
       
       <div className="App">  
        
-            <Header/>
+            <Header name={"Damien Graille"} age={"web developer"}
+            info={info}
+            />
             <Work/>
             <Projects/>
             <Education/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 class Header extends Component {
     render() {
-      return (<div>
+      return (
+      <div>
         <h1 >
-        Damien Graille
+        {this.props.name}
         </h1>
-        <div class="title2">web developer
+        <div class="title2">{this.props.title}
         </div>
+        <h4>
+        Targetting an entry level developer job
+        </h4>
+        <ul><span>
+          {this.props.info.infos.map((data)=> <li>{data}</li>)
+        }
+        </span>
+        <span class="list">
+        {this.props.info.infodata.map((data)=> <li>{data}</li>)
+        }
+        </span>
+        </ul>
       </div>);
     }
   }
